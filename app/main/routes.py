d1cb641d46a4e13dd5f18f5eda4819303b003dc0
Kacper Dezor <48963664+Kaper365@users.noreply.github.com>
2026-04-24 22:16:29 +0200

    Clock task

--- app/main/routes.py
+++ app/main/routes.py
@@ -6,3 +6,25 @@ from zoneinfo import ZoneInfo
 @bp.route('/')
 def home():
     return render_template('home.html')
+
+timezones = {
+    "UTC": "UTC",
+    "New York (US Eastern)": "America/New_York",
+    "Los Angeles (US Pacific)": "America/Los_Angeles",
+    "London (UK)": "Europe/London",
+    "Berlin (Central Europe)": "Europe/Berlin",
+    "Moscow": "Europe/Moscow",
+    "Dubai": "Asia/Dubai",
+    "Mumbai (India)": "Asia/Kolkata",
+    "Beijing (China)": "Asia/Shanghai",
+    "Tokyo (Japan)": "Asia/Tokyo",
+    "Sydney (Australia)": "Australia/Sydney",
+    "São Paulo (Brazil)": "America/Sao_Paulo",
+}
+
+@bp.route('/clock')
+def clock():
+    timezone_ids = [tz for name, tz in timezones.items()]
+    timezone_names = [name for name, tz in timezones.items()]
+    current_times = [datetime.now(ZoneInfo(tz)).isoformat() for tz_name, tz in timezones.items()]
+    return render_template('clock.html', times=lambda: zip(timezone_ids,timezone_names,current_times))
